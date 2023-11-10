605ba038fdfe945548dc0ba581460451b437ab92
liuds832 <89030715+liuds832@users.noreply.github.com>
2023-11-10 17:26:35 +0800

    add activation lock request

---
+++ platform/dep/dep_activation_lock.go
@@ -0,0 +1,59 @@
+package dep
+
+import (
+	"context"
+	"errors"
+	"net/http"
+
+	"github.com/go-kit/kit/endpoint"
+
+	"github.com/liuds832/micromdm/dep"
+	"github.com/liuds832/micromdm/pkg/httputil"
+)
+
+func (svc *DEPService) SetActivationLock(ctx context.Context, p *dep.ActivationLockRequest) (*dep.ActivationLockResponse, error) {
+	if svc.client == nil {
+		return nil, errors.New("DEP not configured yet. add a DEP token to enable DEP")
+	}
+	return svc.client.ActivationLock(p)
+}
+
+type activationLockRequest struct {
+	*dep.ActivationLockRequest
+}
+
+type activationLockResponse struct {
+	*dep.ActivationLockResponse
+	Err error `json:"err,omitempty"`
+}
+
+func (r activationLockResponse) Failed() error { return r.Err }
+
+func decodeActivationLockRequest(ctx context.Context, r *http.Request) (interface{}, error) {
+	var req activationLockRequest
+	err := httputil.DecodeJSONRequest(r, &req)
+	return req, err
+}
+
+func decodeActivationLockResponse(_ context.Context, r *http.Response) (interface{}, error) {
+	var resp activationLockResponse
+	err := httputil.DecodeJSONResponse(r, &resp)
+	return resp, err
+}
+
+func MakeSetActivationLockEndpoint(svc Service) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
+		req := request.(activationLockRequest)
+		resp, err := svc.SetActivationLock(ctx, req.ActivationLockRequest)
+		return activationLockResponse{ActivationLockResponse: details, Err: err}, nil
+	}
+}
+
+func (e Endpoints) SetActivationLock(ctx context.Context, p *dep.ActivationLockRequest) (*dep.ActivationLockResponse, error) {
+	request := activationLockRequest{ActivationLockRequest: p}
+	response, err := e.SetActivationLockEndpoint(ctx, request)
+	if err != nil {
+		return nil, err
+	}
+	return response.(activationLockResponse).ActivationLockResponse, response.(activationLockResponse).Err
+}
--- platform/dep/server.go
+++ platform/dep/server.go
@@ -9,22 +9,24 @@ import (
 )
 
 type Endpoints struct {
-	DefineProfileEndpoint    endpoint.Endpoint
-	FetchProfileEndpoint     endpoint.Endpoint
-	GetAccountInfoEndpoint   endpoint.Endpoint
-	GetDeviceDetailsEndpoint endpoint.Endpoint
-	AssignProfileEndpoint    endpoint.Endpoint
-	RemoveProfileEndpoint    endpoint.Endpoint
+	DefineProfileEndpoint    	endpoint.Endpoint
+	FetchProfileEndpoint     	endpoint.Endpoint
+	GetAccountInfoEndpoint   	endpoint.Endpoint
+	GetDeviceDetailsEndpoint 	endpoint.Endpoint
+	AssignProfileEndpoint    	endpoint.Endpoint
+	RemoveProfileEndpoint    	endpoint.Endpoint
+	SetActivationLockEndpoint	endpoint.Endpoint
 }
 
 func MakeServerEndpoints(s Service, outer endpoint.Middleware, others ...endpoint.Middleware) Endpoints {
 	return Endpoints{
-		AssignProfileEndpoint:    endpoint.Chain(outer, others...)(MakeAssignProfileEndpoint(s)),
-		RemoveProfileEndpoint:    endpoint.Chain(outer, others...)(MakeRemoveProfileEndpoint(s)),
-		DefineProfileEndpoint:    endpoint.Chain(outer, others...)(MakeDefineProfileEndpoint(s)),
-		FetchProfileEndpoint:     endpoint.Chain(outer, others...)(MakeFetchProfileEndpoint(s)),
-		GetAccountInfoEndpoint:   endpoint.Chain(outer, others...)(MakeGetAccountInfoEndpoint(s)),
-		GetDeviceDetailsEndpoint: endpoint.Chain(outer, others...)(MakeGetDeviceDetailsEndpoint(s)),
+		AssignProfileEndpoint:    	endpoint.Chain(outer, others...)(MakeAssignProfileEndpoint(s)),
+		RemoveProfileEndpoint:    	endpoint.Chain(outer, others...)(MakeRemoveProfileEndpoint(s)),
+		DefineProfileEndpoint:    	endpoint.Chain(outer, others...)(MakeDefineProfileEndpoint(s)),
+		FetchProfileEndpoint:     	endpoint.Chain(outer, others...)(MakeFetchProfileEndpoint(s)),
+		GetAccountInfoEndpoint:   	endpoint.Chain(outer, others...)(MakeGetAccountInfoEndpoint(s)),
+		GetDeviceDetailsEndpoint: 	endpoint.Chain(outer, others...)(MakeGetDeviceDetailsEndpoint(s)),
+		SetActivationLockEndpoint:	endpoint.Chain(outer, others...)(MakeSetActivationLockEndpoint(s)),
 	}
 }
 
@@ -35,6 +37,7 @@ func RegisterHTTPHandlers(r *mux.Router, e Endpoints, options ...httptransport.S
 	// POST		/v1/dep/devices			get device details given a list of serials
 	// POST		/v1/dep/assign			assign a specific profile ID to one or more serials
 	// DELETE	/v1/dep/profiles		clear an assigned DEP profile for one or more serials
+	// POST		/v1/dep/activationlock	set device activation lock
 
 	r.Methods("PUT").Path("/v1/dep/profiles").Handler(httptransport.NewServer(
 		e.DefineProfileEndpoint,
@@ -74,6 +77,13 @@ func RegisterHTTPHandlers(r *mux.Router, e Endpoints, options ...httptransport.S
 	r.Methods("DELETE").Path("/v1/dep/profiles").Handler(httptransport.NewServer(
 		e.RemoveProfileEndpoint,
 		decodeRemoveProfileRequest,
+		httputil.EncodeJSONResponse,
+		options...,
+	))
+
+	r.Methods("POST").Path("/v1/dep/activationlock").Handler(httptransport.NewServer(
+		e.SetActivationLockEndpoint,
+		decodeActivationLockRequest,
 		httputil.EncodeJSONResponse,
 		options...,
 	))
