e183e380bc04f28d0c70f8af3ec90705e3de6107
liuds832 <89030715+liuds832@users.noreply.github.com>
2023-12-19 10:09:05 +0800

    debug

--- workflow/webhook/depSync.go
+++ workflow/webhook/depSync.go
@@ -3,16 +3,14 @@ package webhook
 import (
 	"github.com/pkg/errors"
 
-	"github.com/liuds832/micromdm/mdm"
 	"github.com/liuds832/micromdm/platform/dep/sync"
-	"github.com/liuds832/micromdm/dep"
 )
 
 
 func depSyncEvent(topic string, data []byte) (*Event, error) {
 	var ev sync.Event
 	if err := sync.UnmarshalEvent(data, &ev); err != nil {
-		return errors.Wrap(err, "unmarshal depsync event")
+		return nil, errors.Wrap(err, "unmarshal depsync event for webhook")
 	}
 
 	webhookEvent := Event{
